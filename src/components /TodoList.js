f15da6dc7b3d192e172afc0327537355f716c9b1
Kirandeep <kiranaus1777@gmail.com>
2022-07-09 21:23:04 +1000

    Delete function working

--- src/components /TodoList.js
+++ src/components /TodoList.js
@@ -2,14 +2,20 @@ import React from 'react';
 //Import Components 
 import Todo from './Todo';
 
-const TodoList =({todos}) => {
+const TodoList =({todos , setTodos }) => {
     
 
     return(
         <div className = "todo-container">
             <ul className ="todo-list">
-                {todos.map(todo => (
-                    <Todo text={todo.text} id ={todo.id} />
+                {todos.map((todo) => (
+                    <Todo
+                    setTodos ={setTodos} 
+                    todos ={todos}
+                    key ={todo.id}
+                    text={todo.text} 
+                    todo= {todo}
+                    />
                 ))}
                 
             </ul>
